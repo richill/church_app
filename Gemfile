1f0e6b0ea4bcc44cff412c25ffbe62778408605c
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-04-16 16:35:21 +0100

    updated header links and gem file

--- Gemfile
+++ Gemfile
@@ -1,10 +1,5 @@
 source 'https://rubygems.org'
 
-git_source(:github) do |repo_name|
-  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
-  "https://github.com/#{repo_name}.git"
-end
-
 gem 'rails', '~> 5.0.2'
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
@@ -16,6 +11,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
 gem 'font-awesome-rails'
 gem 'foundation-rails'
+gem 'jquery-ui-rails'
 gem 'devise'
 
 group :development, :test do
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,139 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require foundation
+
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require foundation
 //= require turbolinks
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+// [nav links]
+$(document).ready(function() {
+  // [nav links] link_aboutus
+  $(document).ready(function() {
+    $("#link_aboutus").mouseover(function() {
+      $("#linkContainer_aboutus").fadeToggle(300);
+      $("#linkContainer_connect").fadeOut("slow");
+      $("#linkContainer_events").fadeOut("slow");
+      $("#linkContainer_resources").fadeOut("slow");
+      $("#linkContainer_contact").fadeOut("slow");
+      return false;
+    });
+    //Document Click hiding the popup 
+    $(document).click(function() {
+      $("#linkContainer_aboutus").hide();
+    });
+    //Popup on click
+    $("#linkContainer_aboutus").click(function() {
+      return true;
+    });
+  });
+
+  // [nav links] link_serve
+  $(document).ready(function() {
+    $("#link_serve").mouseover(function() {
+      $("#linkContainer_aboutus").fadeOut("slow");
+      $("#linkContainer_connect").fadeOut("slow");
+      $("#linkContainer_events").fadeOut("slow");
+      $("#linkContainer_resources").fadeOut("slow");
+      $("#linkContainer_contact").fadeOut("slow");
+      return false;
+    });
+  });
+
+  // [nav links] link_connect
+  $(document).ready(function() {
+    $("#link_connect").mouseover(function() {
+      $("#linkContainer_connect").fadeToggle(300);
+      $("#linkContainer_events").fadeOut("slow");
+      $("#linkContainer_aboutus").fadeOut("slow");
+      $("#linkContainer_resources").fadeOut("slow");
+      $("#linkContainer_contact").fadeOut("slow");
+      return false;
+    });
+    //Document Click hiding the popup 
+    $(document).click(function() {
+      $("#linkContainer_connect").hide();
+    });
+    //Popup on click
+    $("#linkContainer_connect").click(function() {
+      return true;
+    });
+  });
+
+  // [nav links] link_events
+  $(document).ready(function() {
+    $("#link_events").mouseover(function() {
+      $("#linkContainer_events").fadeToggle(300);
+      $("#linkContainer_aboutus").fadeOut("slow");
+      $("#linkContainer_connect").fadeOut("slow");
+      $("#linkContainer_resources").fadeOut("slow");
+      $("#linkContainer_contact").fadeOut("slow");
+      return false;
+    });
+    //Document Click hiding the popup 
+    $(document).click(function() {
+      $("#linkContainer_events").hide();
+    });
+    //Popup on click
+    $("#linkContainer_events").click(function() {
+      return true;
+    });
+  });
+
+  // [nav links] link_grow
+  $(document).ready(function() {
+    $("#link_grow").mouseover(function() {
+      $("#linkContainer_aboutus").fadeOut("slow");
+      $("#linkContainer_connect").fadeOut("slow");
+      $("#linkContainer_events").fadeOut("slow");
+      $("#linkContainer_resources").fadeOut("slow");
+      $("#linkContainer_contact").fadeOut("slow");
+      return false;
+    });
+  });
+
+  // [nav links] link_resources
+  $(document).ready(function() {
+    $("#link_resources").mouseover(function() {
+      $("#linkContainer_resources").fadeToggle(300);
+      $("#linkContainer_aboutus").fadeOut("slow");
+      $("#linkContainer_connect").fadeOut("slow");
+      $("#linkContainer_events").fadeOut("slow");
+      $("#linkContainer_contact").fadeOut("slow");
+      return false;
+    });
+    //Document Click hiding the popup 
+    $(document).click(function() {
+      $("#linkContainer_resources").hide();
+    });
+    //Popup on click
+    $("#linkContainer_resources").click(function() {
+      return true;
+    });
+  });
+
+  // [nav links] link_contact
+  $(document).ready(function() {
+    $("#link_contact").mouseover(function() {
+      $("#linkContainer_contact").fadeToggle(300);
+      $("#linkContainer_aboutus").fadeOut("slow");
+      $("#linkContainer_connect").fadeOut("slow");
+      $("#linkContainer_events").fadeOut("slow");
+      $("#linkContainer_resources").fadeOut("slow");
+      return false;
+    });
+    //Document Click hiding the popup 
+    $(document).click(function() {
+      $("#linkContainer_contact").hide();
+    });
+    //Popup on click
+    $("#linkContainer_contact").click(function() {
+      return true;
+    });
+  });
+});
