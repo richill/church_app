5a085963cb6b4c69f48980711bc5f23664a47555
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-04-27 15:25:35 +0100

    updated js for menu

---
+++ app/assets/javascripts/responsive_header.js
@@ -0,0 +1,45 @@
+/*===================================================
+    Page: responsive header
+    File: views/shared/_responsive_header.html.erb
+  ===================================================*/
+
+$(document).ready(function(){
+
+
+  $( "#menu_toggle" ).click(function() {
+    $("#menu_wrap").slideDown(300);
+  
+  });
+    
+});
+
+
+$(document).ready(function(){
+
+  $( "#menu_toggle" ).click(function() {
+    $("#menu_wrap").slideDown(300);
+  
+  });
+    
+});
+
+
+// $(document).ready(function(){
+//     $(".rep_content").toggle();
+//     $(".fa-bars").click(function(){
+//         $(".rep_content").toggle();
+//     });
+// });
+
+
+// $( ".fa-bars" ).click(function() {
+//   $( ".menu_wrap" ).slideDown( "slow", function() {
+//     // Animation complete.
+//   });
+// });
+
+// $( "#clickme" ).click(function() {
+//   $( "#book" ).slideDown( "slow", function() {
+//     // Animation complete.
+//   });
+// });
