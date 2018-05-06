798f2da9b3d8ddd8bf16aab1075fc6a0b409316c
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-05-06 16:57:38 +0100

    added validations

--- app/models/launchministry.rb
+++ app/models/launchministry.rb
@@ -1,2 +1,13 @@
 class Launchministry < ApplicationRecord
+  validates_presence_of :firstname, presence: true, message: "can't be blank"
+  validates_presence_of :lastname, presence: true, message: "can't be blank"
+  validates_presence_of :email, presence: true, message: "can't be blank"
+  validates_presence_of :mobile, presence: true, message: "can't be blank"
+  validates_presence_of :address, presence: true, message: "can't be blank"
+  validates_presence_of :city, presence: true, message: "can't be blank"
+  validates_presence_of :postcode, presence: true, message: "can't be blank"
+  validates_presence_of :mininstryidea, presence: true, message: "can't be blank"
+  validates_presence_of :mininstrydescription, presence: true, message: "can't be blank"
+  validates_presence_of :ministryneed, presence: true, message: "can't be blank"
+  validates_presence_of :ministrymission, presence: true, message: "can't be blank"
 end
