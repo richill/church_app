dc56aa8c3d968b3323f8b20f91ef67a0dac9a7c2
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-05-08 20:25:28 +0100

    updated event page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :start_time, :end_time, :category_event_id, :description, :address, :city, :postcode, :link, :image, :category_country_id)
+      params.require(:event).permit(:name, :start_time, :end_time, :category_event_id, :description, :address, :city, :postcode, :link, :image, :category_country_id, :linkmap)
     end
 end
