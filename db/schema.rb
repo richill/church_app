d32b2fe7793b131a41b4cfd219b9665d199efcf0
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-05-08 17:44:14 +0100

    generated events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180505164539) do
+ActiveRecord::Schema.define(version: 20180508164314) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "launchministries", force: :cascade do |t|
     t.string   "firstname"
