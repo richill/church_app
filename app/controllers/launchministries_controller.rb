70e89d838d9e449a91089cc8abad1daaeb21f431
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-05-13 12:40:43 +0100

    updated controllers with friendlyID

--- app/controllers/launchministries_controller.rb
+++ app/controllers/launchministries_controller.rb
@@ -68,7 +68,7 @@ class LaunchministriesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_launchministry
-      @launchministry = Launchministry.find(params[:id])
+      @launchministry = Launchministry.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
