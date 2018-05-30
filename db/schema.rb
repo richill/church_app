7b1f1e90541c6bc1a69310c1361e2ec29d2efb05
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-05-30 14:04:38 +0100

    added job to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180520125133) do
+ActiveRecord::Schema.define(version: 20180530130418) do
 
   create_table "category_countries", force: :cascade do |t|
     t.string   "name"
@@ -166,6 +166,7 @@ ActiveRecord::Schema.define(version: 20180520125133) do
     t.boolean  "admin"
     t.string   "slug"
     t.boolean  "approve"
+    t.string   "job"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
