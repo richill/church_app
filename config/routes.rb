40dbb7b92c452730ca24e35f1f54b4c6375c1517
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-06-04 14:21:58 +0100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :jobs
   resources :smallgroups
   resources :recommendedreadings
+  resources :attending_groups, only: [:create, :destroy]
+
   resources :discussions do
     resources :comments, only: [:create, :destroy] 
   end
