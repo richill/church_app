28d93f95ec13319d88352b9948ce3e92664b2b4d
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-06-04 14:20:26 +0100

    updated smallgroup model

--- app/models/smallgroup.rb
+++ app/models/smallgroup.rb
@@ -2,6 +2,8 @@ class Smallgroup < ApplicationRecord
   extend FriendlyId
   friendly_id :slug_smallgroups, use: :slugged
 
+  has_many :attendances, as: :attendable, dependent: :destroy
+
   def slug_smallgroups
     [
       :name
