04cb7b2bc7887dff9a480d63fbe43b94827414be
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-06-04 14:27:03 +0100

    updated attendance controller

--- app/controllers/attending_groups_controller.rb
+++ app/controllers/attending_groups_controller.rb
@@ -1,2 +1,26 @@
 class AttendingGroupsController < ApplicationController
+  before_action :authenticate_user!, :set_smallgroup
+
+  def create
+    if current_user.user_signed_in?
+      if Attendance.find_or_create_by(attendable: @smallgroup, user: current_user)
+        redirect_to :back, notice: 'Joined Group'
+      else
+        redirect_to :back, alert: 'Something went wrong...*sad panda*'
+      end
+    else
+      redirect_to subscription_path(@premium_plan)
+    end
+  end
+
+  
+  def destroy
+    Attendance.where(attendable_id: @social.id, user_id: current_user.id).first.destroy
+    redirect_to :back, notice: 'Left Group'
+  end
+  
+  private
+  def set_smallgroup
+    @smallgroup = Smallgroup.friendly.find(params[:smallgroup_id] || params[:id])
+  end
 end
