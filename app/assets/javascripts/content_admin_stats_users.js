0028ddf6bd930502362806c951bfad838237ea06
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-06-15 14:54:06 +0100

    updated js for admin user stats

--- app/assets/javascripts/content_admin_stats_users.js
+++ app/assets/javascripts/content_admin_stats_users.js
@@ -4,31 +4,34 @@
   ===================================================*/
 
 $(document).ready(function() {
-  $('.userstats_user_tab').click(function() {
-    $(".userstats_user_tab_content").show();
-    $(".userstats_males_tab_content").hide();
-    $(".userstats_females_tab_content").hide();
-    $(".userstats_admins_tab_content").hide();
+  $("#userstats_males_tab_content").hide();
+  $("#userstats_females_tab_content").hide();
+  $("#userstats_admins_tab_content").hide();
+  $('#userstats_user_tab').click(function() {
+    $("#userstats_user_tab_content").show();
+    $("#userstats_males_tab_content").hide();
+    $("#userstats_females_tab_content").hide();
+    $("#userstats_admins_tab_content").hide();
   });
 
-  $('.userstats_males_tab').click(function() {
-    $(".userstats_males_tab_content").show();
-    $(".userstats_user_tab_content").hide();
-    $(".userstats_females_tab_content").hide();
-    $(".userstats_admins_tab_content").hide();
+  $('#userstats_males_tab').click(function() {
+    $("#userstats_males_tab_content").show();
+    $("#userstats_user_tab_content").hide();
+    $("#userstats_females_tab_content").hide();
+    $("#userstats_admins_tab_content").hide();
   });
 
-  $('.userstats_females_tab').click(function() {
-    $(".userstats_females_tab_content").show();
-    $(".userstats_males_tab_content").hide();
-    $(".userstats_user_tab_content").hide();
-    $(".userstats_admins_tab_content").hide();
+  $('#userstats_females_tab').click(function() {
+    $("#userstats_females_tab_content").show();
+    $("#userstats_males_tab_content").hide();
+    $("#userstats_user_tab_content").hide();
+    $("#userstats_admins_tab_content").hide();
   });
 
-  $('.userstats_admins_tab').click(function() {
-    $(".userstats_admins_tab_content").show();
-    $(".userstats_males_tab_content").hide();
-    $(".userstats_females_tab_content").hide();
-    $(".userstats_user_tab_content").hide();
+  $('#userstats_admins_tab').click(function() {
+    $("#userstats_admins_tab_content").show();
+    $("#userstats_males_tab_content").hide();
+    $("#userstats_females_tab_content").hide();
+    $("#userstats_user_tab_content").hide();
   });
 });
