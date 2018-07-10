82b58be206886557d95b36d7f4d4365986ddd79d
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-07-10 14:38:41 +0100

    updated listed min

---
+++ app/assets/javascripts/content_admin_stats_ministries.js
@@ -0,0 +1,37 @@
+/*===================================================
+    Page: admin panel/stats ministries page
+    File: views/users/shared/_content_admin_stats_ministries.html.erb
+  ===================================================*/
+
+$(document).ready(function() {
+  $("#userstats_males_tab_content").hide();
+  $("#userstats_females_tab_content").hide();
+  $("#userstats_admins_tab_content").hide();
+  $('#userstats_user_tab').click(function() {
+    $("#userstats_user_tab_content").show();
+    $("#userstats_males_tab_content").hide();
+    $("#userstats_females_tab_content").hide();
+    $("#userstats_admins_tab_content").hide();
+  });
+
+  $('#userstats_males_tab').click(function() {
+    $("#userstats_males_tab_content").show();
+    $("#userstats_user_tab_content").hide();
+    $("#userstats_females_tab_content").hide();
+    $("#userstats_admins_tab_content").hide();
+  });
+
+  $('#userstats_females_tab').click(function() {
+    $("#userstats_females_tab_content").show();
+    $("#userstats_males_tab_content").hide();
+    $("#userstats_user_tab_content").hide();
+    $("#userstats_admins_tab_content").hide();
+  });
+
+  $('#userstats_admins_tab').click(function() {
+    $("#userstats_admins_tab_content").show();
+    $("#userstats_males_tab_content").hide();
+    $("#userstats_females_tab_content").hide();
+    $("#userstats_user_tab_content").hide();
+  });
+});
