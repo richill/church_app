3924f45f85290ae16f03e3db3cf1d7219e2a773f
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-07-20 14:11:43 +0100

    updated events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,44 +9,79 @@ class EventsController < ApplicationController
   end
 
   def new
-    @event = Event.new
+    #@event = Event.new
+    @user = current_user
+    @event = @user.events.build
   end
 
   def edit
   end
 
+  # def create
+  #   @event = Event.new(event_params)
+
+  #   respond_to do |format|
+  #     if @event.save
+  #       format.html { redirect_to @event, notice: 'Event was successfully created.' }
+  #       format.json { render :show, status: :created, location: @event }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @event.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
   def create
-    @event = Event.new(event_params)
+    @user = current_user
+    @event = @user.events.create(event_params)
 
     respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+        if @event.save
+          format.html { redirect_to stats_events_user_path(@user), notice: 'Event was successfully created.' }
+          format.json { render :show, status: :created, location: @event }
+        else
+          format.html { render :new }
+          format.json { render json: @event.errors, status: :unprocessable_entity }
+        end
       end
-    end
   end
 
+  # def update
+  #   respond_to do |format|
+  #     if @event.update(event_params)
+  #       format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @event }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @event.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
   def update
+    @user = current_user
+
     respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+        if @event.update_attributes(event_params)
+          format.html { redirect_to stats_events_user_path(@user), notice: 'Event was successfully updated.' }
+          format.json { render :show, status: :ok, location: @event }
+        else
+          format.html { render :edit }
+          format.json { render json: @event.errors, status: :unprocessable_entity }
+        end
       end
-    end
   end
 
+  # def destroy
+  #   @event.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
   def destroy
+    @user = current_user
+    @event = @user.events.friendly.find(params[:id])
     @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to stats_events_user_path(@user)
   end
 
   def calendareventspg
@@ -75,6 +110,6 @@ class EventsController < ApplicationController
     end
 
     def event_params
-      params.require(:event).permit(:name, :start_time, :end_time, :category_event_id, :description, :address, :city, :postcode, :link, :image, :category_country_id, :linkmap)
+      params.require(:event).permit(:name, :start_time, :end_time, :category_event_id, :description, :address, :city, :postcode, :link, :image, :category_country_id, :linkmap, :user_id)
     end
 end
