bd57d603c61b45896169607d58f33cbf74e68c4c
Richill Tamakloe <richill.tamakloe@gmail.com>
2018-10-20 16:04:35 +0100

    updated image loader file

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,23 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave
+
+  process :convert => 'png'
+  process :tags => ['post_picture']
+  
+  version :standard do
+    process :resize_to_fill => [100, 150, :north]
+  end
+  
+  version :thumbnail do
+    resize_to_fit(50, 50)
+  end
+  
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
