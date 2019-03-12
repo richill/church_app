e3a0f71faa4da35aa7c3ebaf53498b46bd794f5e
Richill Tamakloe <richill.tamakloe@gmail.com>
2019-03-12 17:34:17 +0000

    updated

--- app/controllers/launchministries_controller.rb
+++ app/controllers/launchministries_controller.rb
@@ -3,78 +3,99 @@ class LaunchministriesController < ApplicationController
   before_filter :setup_generic
 
   def index
-    @launchministries = Launchministry.all
+    if user_signed_in? && current_user.admin
+      @launchministries = Launchministry.all
+    else
+      redirect_to error_path
+    end
   end
 
   def show
   end
 
   def new
-    @user = current_user
-    @launchministry = @user.launchministries.build
+    if user_signed_in?
+      @user = current_user
+      @launchministry = @user.launchministries.build
+    else
+      redirect_to error_path
+    end
   end
 
   def edit
+    unless user_signed_in? && current_user.admin
+      redirect_to error_path
+    end
   end
 
   def create
-    @user = current_user
-    @launchministry = @user.launchministries.create(launchministry_params)
-
-    if @user.admin?
-      respond_to do |format|
-        if @launchministry.save
-          format.html { redirect_to stats_ministries_user_path(@user), notice: 'Ministry was successfully created.' }
-          format.json { render :show, status: :created, location: @launchministry }
-        else
-          format.html { render :new }
-          format.json { render json: @launchministry.errors, status: :unprocessable_entity }
+    if user_signed_in?
+      @user = current_user
+      @launchministry = @user.launchministries.create(launchministry_params)
+      if @user.admin?
+        respond_to do |format|
+          if @launchministry.save
+            format.html { redirect_to stats_ministries_user_path(@user), notice: 'Ministry was successfully created.' }
+            format.json { render :show, status: :created, location: @launchministry }
+          else
+            format.html { render :new }
+            format.json { render json: @launchministry.errors, status: :unprocessable_entity }
+          end
+        end
+      else
+        respond_to do |format|
+          if @launchministry.save
+            format.html { redirect_to thank_you_path, notice: 'Ministry was successfully created.' }
+            format.json { render :show, status: :created, location: @launchministry }
+          else
+            format.html { render :new }
+            format.json { render json: @launchministry.errors, status: :unprocessable_entity }
+          end
         end
       end
     else
-      respond_to do |format|
-        if @launchministry.save
-          format.html { redirect_to thank_you_path, notice: 'Ministry was successfully created.' }
-          format.json { render :show, status: :created, location: @launchministry }
-        else
-          format.html { render :new }
-          format.json { render json: @launchministry.errors, status: :unprocessable_entity }
-        end
-      end
+      redirect_to error_path
     end
   end
 
   def update
-    @user = current_user
-
-    if @user.admin?
-      respond_to do |format|
-        if @launchministry.update_attributes(launchministry_params)
-          format.html { redirect_to stats_ministries_user_path(@user), notice: 'Launchministry was successfully updated.' }
-          format.json { render :show, status: :ok, location: @launchministry }
-        else
-          format.html { render :edit }
-          format.json { render json: @launchministry.errors, status: :unprocessable_entity }
+    if user_signed_in? && current_user.admin
+      @user = current_user
+      if @user.admin?
+        respond_to do |format|
+          if @launchministry.update_attributes(launchministry_params)
+            format.html { redirect_to stats_ministries_user_path(@user), notice: 'Launchministry was successfully updated.' }
+            format.json { render :show, status: :ok, location: @launchministry }
+          else
+            format.html { render :edit }
+            format.json { render json: @launchministry.errors, status: :unprocessable_entity }
+          end
+        end
+      else
+        respond_to do |format|
+          if @launchministry.update_attributes(launchministry_params)
+            format.html { redirect_to @launchministry, notice: 'Launchministry was successfully updated.' }
+            format.json { render :show, status: :ok, location: @launchministry }
+          else
+            format.html { render :edit }
+            format.json { render json: @launchministry.errors, status: :unprocessable_entity }
+          end
         end
       end
     else
-      respond_to do |format|
-        if @launchministry.update_attributes(launchministry_params)
-          format.html { redirect_to @launchministry, notice: 'Launchministry was successfully updated.' }
-          format.json { render :show, status: :ok, location: @launchministry }
-        else
-          format.html { render :edit }
-          format.json { render json: @launchministry.errors, status: :unprocessable_entity }
-        end
-      end
+      redirect_to error_path
     end
   end
 
   def destroy
-    @user = current_user
-    @launchministry = @user.launchministries.friendly.find(params[:id])
-    @launchministry.destroy
-    redirect_to stats_ministries_user_path(@user)
+    if user_signed_in? && current_user.admin
+      @user = current_user
+      @launchministry = @user.launchministries.friendly.find(params[:id])
+      @launchministry.destroy
+      redirect_to stats_ministries_user_path(@user)
+    else
+      redirect_to error_path
+    end
   end
 
   def ministriespg
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,52 +2,79 @@ class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
 
   def index
-    @photos = Photo.all
+    if user_signed_in? && current_user.admin
+      @photos = Photo.all
+    else
+      redirect_to error_path
+    end
   end
 
   def show
+    unless user_signed_in? && current_user.admin
+      redirect_to error_path
+    end
   end
 
   def new
-    @gallery = Gallery.friendly.find(params[:gallery_id])
-    @photo = @gallery.photos.build
+    if user_signed_in? && current_user.admin
+      @gallery = Gallery.friendly.find(params[:gallery_id])
+      @photo = @gallery.photos.build
+    else
+      redirect_to error_path
+    end
   end
 
   def edit
-    @gallery = gallery.friendly.find(params[:gallery_id])
+    if user_signed_in? && current_user.admin
+      @gallery = gallery.friendly.find(params[:gallery_id])
+    else
+      redirect_to error_path
+    end
   end
 
   def create
-    @gallery = Gallery.friendly.find(params[:gallery_id])
-    @photo = @gallery.photos.create(photo_params)
-    respond_to do |format|
-      if @photo.save
-        format.html { redirect_to(gallery_path(@gallery), notice: 'photo was successfully created.') }
-        format.json  { render json: @photo, status: :created, location: @photo }
-      else
-        format.html { render action: "new" }
-        format.json  { render json: @photo.errors, status: :unprocessable_entity }
+    if user_signed_in? && current_user.admin
+      @gallery = Gallery.friendly.find(params[:gallery_id])
+      @photo = @gallery.photos.create(photo_params)
+      respond_to do |format|
+        if @photo.save
+          format.html { redirect_to(gallery_path(@gallery), notice: 'photo was successfully created.') }
+          format.json  { render json: @photo, status: :created, location: @photo }
+        else
+          format.html { render action: "new" }
+          format.json  { render json: @photo.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to error_path
     end
   end
 
   def update
-    respond_to do |format|
-      if @photo.update_attributes(photo_params)
-        format.html { redirect_to([@photo.gallery, @photo], notice: 'photo was successfully updated.') }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
+    if user_signed_in? && current_user.admin
+      respond_to do |format|
+        if @photo.update_attributes(photo_params)
+          format.html { redirect_to([@photo.gallery, @photo], notice: 'photo was successfully updated.') }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @photo.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to error_path
     end
   end
 
   def destroy
-    @gallery = Gallery.friendly.find(params[:gallery_id])
-    @photo = @gallery.photos.find(params[:id])
-    @photo.destroy
-    redirect_to photos_path
+    if user_signed_in? && current_user.admin
+      @gallery = Gallery.friendly.find(params[:gallery_id])
+      @photo = @gallery.photos.find(params[:id])
+      @photo.destroy
+      redirect_to photos_path
+    else
+      redirect_to error_path
+    end
   end
 
 
