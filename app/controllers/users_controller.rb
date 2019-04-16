77804931a1b8ad74866fd41cbb8ce1f703d437fd
Richill Tamakloe <richill.tamakloe@gmail.com>
2019-04-16 23:01:05 +0100

    updated admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,13 +49,25 @@ class UsersController < ApplicationController
 
   def update
     if user_signed_in? && current_user.admin || user_signed_in? && @user == current_user
-      respond_to do |format|
-        if @user.update(user_params)
-          format.html { redirect_to @user, notice: 'User was successfully updated.' }
-          format.json { render :show, status: :ok, location: @user }
-        else
-          format.html { render :edit }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
+      if current_user.admin
+        respond_to do |format|
+          if @user.update(user_params)
+            format.html { redirect_to stats_users_user_path(current_user), notice: 'User was successfully updated.' }
+            format.json { render :show, status: :ok, location: @user }
+          else
+            format.html { render :edit }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+          end
+        end
+      else
+        respond_to do |format|
+          if @user.update(user_params)
+            format.html { redirect_to @user, notice: 'User was successfully updated.' }
+            format.json { render :show, status: :ok, location: @user }
+          else
+            format.html { render :edit }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+          end
         end
       end
     else
