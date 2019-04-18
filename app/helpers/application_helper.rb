2715c1310839d97a9b531773de7fb61cde888c72
Richill Tamakloe <richill.tamakloe@gmail.com>
2019-04-18 15:47:27 +0100

    updated video display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  # helper to extract youtube video_id
+  def embed(link)
+    # youtube_id = link.try(:split, ",").last
+    youtube_id = link.split("=").last
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+  end
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Open Heavens"
+    if page_title.empty?
+      base_title
+    else
+      base_title + " | " + page_title
+    end
+  end
 end
