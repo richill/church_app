4c5161d7845595a43d9dd986e4eda4fc3b0fbf36
Richill Tamakloe <richill.tamakloe@gmail.com>
2019-04-18 12:25:24 +0100

    updated sitemgt access

--- app/controllers/sitemanagments_controller.rb
+++ app/controllers/sitemanagments_controller.rb
@@ -1,73 +1,84 @@
 class SitemanagmentsController < ApplicationController
   before_action :set_sitemanagment, only: [:show, :edit, :update, :destroy]
 
-  # GET /sitemanagments
-  # GET /sitemanagments.json
   def index
-    @sitemanagments = Sitemanagment.all
+    if user_signed_in? && current_user.admin
+      @sitemanagments = Sitemanagment.all
+    else
+      redirect_to error_path
+    end
   end
 
-  # GET /sitemanagments/1
-  # GET /sitemanagments/1.json
   def show
+    unless user_signed_in? && current_user.admin
+      redirect_to error_path
+    end
   end
 
-  # GET /sitemanagments/new
   def new
-    @sitemanagment = Sitemanagment.new
+    if user_signed_in? && current_user.admin
+      @sitemanagment = Sitemanagment.new
+    else
+      redirect_to error_path
+    end
   end
 
-  # GET /sitemanagments/1/edit
   def edit
+    unless user_signed_in? && current_user.admin
+      redirect_to error_path
+    end
   end
 
-  # POST /sitemanagments
-  # POST /sitemanagments.json
   def create
-    @sitemanagment = Sitemanagment.new(sitemanagment_params)
-
-    respond_to do |format|
-      if @sitemanagment.save
-        format.html { redirect_to @sitemanagment, notice: 'Sitemanagment was successfully created.' }
-        format.json { render :show, status: :created, location: @sitemanagment }
-      else
-        format.html { render :new }
-        format.json { render json: @sitemanagment.errors, status: :unprocessable_entity }
+    if user_signed_in? && current_user.admin
+      @sitemanagment = Sitemanagment.new(sitemanagment_params)
+      respond_to do |format|
+        if @sitemanagment.save
+          format.html { redirect_to @sitemanagment, notice: 'Sitemanagment was successfully created.' }
+          format.json { render :show, status: :created, location: @sitemanagment }
+        else
+          format.html { render :new }
+          format.json { render json: @sitemanagment.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to error_path
     end
   end
 
-  # PATCH/PUT /sitemanagments/1
-  # PATCH/PUT /sitemanagments/1.json
   def update
-    respond_to do |format|
-      if @sitemanagment.update(sitemanagment_params)
-        format.html { redirect_to @sitemanagment, notice: 'Sitemanagment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @sitemanagment }
-      else
-        format.html { render :edit }
-        format.json { render json: @sitemanagment.errors, status: :unprocessable_entity }
+    if user_signed_in? && current_user.admin
+      respond_to do |format|
+        if @sitemanagment.update(sitemanagment_params)
+          format.html { redirect_to @sitemanagment, notice: 'Sitemanagment was successfully updated.' }
+          format.json { render :show, status: :ok, location: @sitemanagment }
+        else
+          format.html { render :edit }
+          format.json { render json: @sitemanagment.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to error_path
     end
   end
 
-  # DELETE /sitemanagments/1
-  # DELETE /sitemanagments/1.json
   def destroy
-    @sitemanagment.destroy
-    respond_to do |format|
-      format.html { redirect_to sitemanagments_url, notice: 'Sitemanagment was successfully destroyed.' }
-      format.json { head :no_content }
+    if user_signed_in? && current_user.admin
+      @sitemanagment.destroy
+      respond_to do |format|
+        format.html { redirect_to sitemanagments_url, notice: 'Sitemanagment was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to error_path
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_sitemanagment
       @sitemanagment = Sitemanagment.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def sitemanagment_params
       params.require(:sitemanagment).permit(:homepg_img01, :homepg_img02, :homepg_img03, :homepg_video)
     end
